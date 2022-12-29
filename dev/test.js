2918a9b5aa03581ed49b24e384a08dcb8099f2f9
Ianphantom <ian25yola@gmail.com>
2022-12-30 02:05:06 +0700

    Method Get Last Block

---
+++ dev/test.js
@@ -0,0 +1,7 @@
+const Blockchain = require("./blockchain");
+
+const bitcoin = new Blockchain();
+bitcoin.createNewBlock(1231, "IANFELIXJOANTHANS", "IANFELIXHJONAHHT");
+bitcoin.createNewBlock(1231, "IANFELIXJOANTHANS", "IANFELIXHJONAHHT");
+
+console.log(bitcoin);
