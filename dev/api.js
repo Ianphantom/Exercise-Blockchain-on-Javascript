c34450e05bf9b2b93c5ff05cf27608b6a3555b4d
Ianphantom <ian25yola@gmail.com>
2022-12-30 04:13:06 +0700

    Blockchain API get method

---
+++ dev/api.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const Blockchain = require("./blockchain");
+
+const bitcoin = new Blockchain();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/blockchain", function (req, res) {
+  res.send(bitcoin);
+});
+
+app.post("/transaction", function (req, res) {});
+
+app.get("/mine", function (req, res) {});
+
+app.listen(3000, function () {
+  console.log("listening on port 3000");
+});
