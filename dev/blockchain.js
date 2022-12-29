fef0771081a7dce7cf80f379bbb1d0bfb492fe10
Ianphantom <ian25yola@gmail.com>
2022-12-30 03:30:16 +0700

    ProofOfWork Method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,3 +1,5 @@
+const sha256 = require("sha256");
+
 function Blockchain() {
   this.chain = [];
   this.pendingTransactions = [];
@@ -40,6 +42,32 @@ Blockchain.prototype.createNewTransaction = function (
 
   this.pendingTransactions.push(newTransaction);
   return this.getLastBlock()["index"] + 1;
+};
+
+Blockchain.prototype.hashBlock = function (
+  previousBlockHash,
+  currentBlockData,
+  nonce
+) {
+  const dataAsString =
+    previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+  const hash = sha256(dataAsString);
+
+  return hash;
+};
+
+Blockchain.prototype.proofOfWork = function (
+  previousBlockHash,
+  currentBlockData
+) {
+  let nonce = 0;
+  let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+  while (hash.substring(0, 4) !== "0000") {
+    nonce++;
+    hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+  }
+
+  return nonce;
 };
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,8 +1,25 @@
 const Blockchain = require("./blockchain");
-
 const bitcoin = new Blockchain();
-bitcoin.createNewBlock(1231, "IANFELIXJOANTHANS", "IANFELIXHJONAHHT");
-bitcoin.createNewTransaction(23424, "IANFELIXJOANTHAN", "JONATHANSIMANJUNTAK");
-bitcoin.createNewBlock(1231, "FEWLIXJONATRHAN", "FELIXJONATHAN");
 
-console.log(bitcoin);
+const previousBlockHash = "8907as9d8f79as8d79as8df7a9s7f";
+const currentBlockData = [
+  {
+    amount: 10,
+    sender: "IANFELIX",
+    recipient: "asdfhjkasdhjfkasdfhks",
+  },
+  {
+    amount: 10,
+    sender: "IANFELIX",
+    recipient: "asdfhjkasdhjfkasdfhks",
+  },
+  {
+    amount: 10,
+    sender: "IANFELIX",
+    recipient: "asdfhjkasdhjfkasdfhks",
+  },
+];
+
+// console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
+
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 157233));
