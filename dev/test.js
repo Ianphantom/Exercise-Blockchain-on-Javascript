769b8677e4629d59b9f93db6a7767d43886d1a5d
Ianphantom <ian25yola@gmail.com>
2022-12-30 02:18:38 +0700

    CrateNewTransaction

--- dev/test.js
+++ dev/test.js
@@ -2,6 +2,7 @@ const Blockchain = require("./blockchain");
 
 const bitcoin = new Blockchain();
 bitcoin.createNewBlock(1231, "IANFELIXJOANTHANS", "IANFELIXHJONAHHT");
-bitcoin.createNewBlock(1231, "IANFELIXJOANTHANS", "IANFELIXHJONAHHT");
+bitcoin.createNewTransaction(23424, "IANFELIXJOANTHAN", "JONATHANSIMANJUNTAK");
+bitcoin.createNewBlock(1231, "FEWLIXJONATRHAN", "FELIXJONATHAN");
 
 console.log(bitcoin);
