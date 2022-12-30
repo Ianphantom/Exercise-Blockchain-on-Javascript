a22db3bda2f7fb87879826adafc73dd6b20a5a3b
Ianphantom <ian25yola@gmail.com>
2022-12-30 23:03:03 +0700

    Mine API Endpoint Method

--- dev/api.js
+++ dev/api.js
@@ -1,9 +1,12 @@
+const { v4: uuidv4 } = require("uuid");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+
 const Blockchain = require("./blockchain");
 
 const bitcoin = new Blockchain();
+const nodeAddress = uuidv4().split("-").join("");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,7 +25,26 @@ app.post("/transaction", function (req, res) {
   res.json({ note: `Transaction will be added in block ${blockIndex}` });
 });
 
-app.get("/mine", function (req, res) {});
+app.get("/mine", function (req, res) {
+  const lastBlock = bitcoin.getLastBlock();
+  const hash = lastBlock["hash"];
+
+  const currentBlockData = {
+    transaction: bitcoin.pendingTransactions,
+    index: lastBlock["index"] + 1,
+  };
+
+  const nonce = bitcoin.proofOfWork(lastBlock, currentBlockData);
+  const blockcHash = bitcoin.hashBlock(lastBlock, currentBlockData, nonce);
+  const newBlock = bitcoin.createNewBlock(nonce, hash, blockcHash);
+
+  bitcoin.createNewTransaction(12.5, "00", nodeAddress);
+
+  res.json({
+    note: "New Block mined succesfuully",
+    block: newBlock,
+  });
+});
 
 app.listen(3000, function () {
   console.log("listening on port 3000");
--- dev/test.js
+++ dev/test.js
@@ -1,27 +1,18 @@
 const Blockchain = require("./blockchain");
 const bitcoin = new Blockchain();
 
-const previousBlockHash = "8907as9d8f79as8d79as8df7a9s7f";
+const previousBlockHash =
+  "000063f228a4810b59e80ee0deb63b2a2effd596465adce7c5f74deab0de80d3";
 const currentBlockData = [
   {
-    amount: 10,
-    sender: "IANFELIX",
-    recipient: "asdfhjkasdhjfkasdfhks",
-  },
-  {
-    amount: 10,
-    sender: "IANFELIX",
-    recipient: "asdfhjkasdhjfkasdfhks",
-  },
-  {
-    amount: 10,
-    sender: "IANFELIX",
-    recipient: "asdfhjkasdhjfkasdfhks",
+    amount: "123",
+    sender: "ianfelixjonathan",
+    recipient: "joshuabutar",
   },
 ];
 
 // console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
 
-// console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 157233));
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 102066));
 
-console.log(bitcoin);
+// console.log(bitcoin);
